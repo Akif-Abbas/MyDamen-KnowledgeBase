498428f5bc30c1be23e8950272f897576569a6ba
Akif-Abbas <akif.abbas25@gmail.com>
2026-04-23 15:11:23 +0200

    update permissions and base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyDamen-KnowledgeBase/', // Add this line!
+  base: '/MyDamen-KnowledgeBase/',
   plugins: [react()],
-});
-
+})
